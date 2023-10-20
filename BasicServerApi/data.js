d3fa4b9a77144117878da4df831d295e2ca56a8a
Malik Ahad Ali <ahad.ali@focusteck.com>
2023-10-20 13:03:39 +0500

    BasicServerApi:Completed

---
+++ BasicServerApi/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        name:'Malik Ahad',
+        Des:"ASE",
+        company:'Focusteck'
+    },
+    {
+        name:'Zeeshan',
+        Des:"ASE",
+        company:'Focusteck'
+    },
+    {
+        name:'Muhammad Saad',
+        Des:"DevOps",
+        company:'Focusteck'
+    }
+]
+
+module.exports = data;
